8d19f7d5e27d0f0427f66e51d643ac2734576b98
Liam Fiddler <liam@mentallyfriendly.com.au>
2015-07-15 10:10:10 +1000

    Added comments to the script and modified the output documentation to improve display within GitHub Wikis

--- phpdoc-to-githubwiki.sh
+++ phpdoc-to-githubwiki.sh
@@ -6,29 +6,48 @@ while true; do
 	read -p "Update the wiki for $github_username/$github_repo? [y/n]: " yn
 	case $yn in
 		[Yy]* ) 
+
+			# create & switch to the temporary directory
 			current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-			temp_dir="temp_$current_time"
-			mkdir $temp_dir
+			temp_dir=`mktemp -d "${TMPDIR:-/tmp}"/phpdocmd.XXXX`
 			cd $temp_dir
+
+			# clone & update the repos we need
 			git clone "https://github.com/$github_username/$github_repo.git"
 			git clone "https://github.com/$github_username/$github_repo.wiki.git"
 			git clone https://github.com/evert/phpdoc-md.git
 			cd phpdoc-md
 			composer update --no-dev
 			cd ..
+
+			# run PHPDocumentor and generate .MD files
 			phpdoc -d $github_repo -t docs/ --template="xml"
+			chmod -R 777 "$github_repo.wiki"
 			rm -r "$github_repo.wiki/*"
 			phpdoc-md/bin/phpdocmd docs/structure.xml "$github_repo.wiki"
+
+			# switch to the Wiki repo dir
 			cd "$github_repo.wiki"
+
+			# remove the .MD extension from URLs so GitHub links them correctly
+			sed -i '' 's/.md)/)/g' *.md
+
+			# remove the HTML P tags from the comments
+			sed -i '' 's/&lt;p&gt;//g' *.md
+			sed -i '' 's/&lt;\/p&gt;//g' *.md
+
+			# update the Wiki repo
 			git add .
 			git commit -am "Updating documentation to match repo as at $current_time"
 			git push
+
+			# clean up
 			cd ..
 			chmod -R 777 $temp_dir
 			rm -r $temp_dir
+
 			exit;;
 		[Nn]* ) exit;;
 		* ) echo "Please answer yes or no.";;
 	esac
 done
-
